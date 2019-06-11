737276c31c915537b4eaf6a6b97cb4c1a842012f
Mick Evans <codemastermick@gmail.com>
2019-06-10 20:27:40 -0400

    feat: add firebase support

---
+++ src/app/firebaseconfig.ts
@@ -0,0 +1,10 @@
+// this file is an example...need to be renamed in firebaseconfig.ts with firebase credentials
+export const firebaseConfig = {
+  apiKey: "AIzaSyB1z5Ssv3FLaRs7gCnD2vwosXbHGz98-_4",
+  authDomain: "dalywinterllp.firebaseapp.com",
+  databaseURL: "https://dalywinterllp.firebaseio.com",
+  projectId: "dalywinterllp",
+  storageBucket: "dalywinterllp.appspot.com",
+  messagingSenderId: "350417584170",
+  appId: "1:350417584170:web:afafe8acccdd8dbe"
+};
