6927918dcd82e810c75a93db6f9e59b12285609d
Mick Evans <codemastermick@gmail.com>
2019-08-08 20:57:58 -0400

    fix: forgot to change import statements

--- src/app/pages/testimonials/testimonials.component.spec.ts
+++ src/app/pages/testimonials/testimonials.component.spec.ts
@@ -1,11 +1,11 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { TestimonialsComponent } from "./testimonials.component";
-import { MaterialModule } from "src/app/material/material.module";
+import { MaterialModule } from "app/material/material.module";
 import { TestimonialsRoutingModule } from "./testimonials-routing.module";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { AngularFireModule } from "@angular/fire";
-import { firebaseConfig } from "src/app/firebaseconfig";
+import { firebaseConfig } from "app/firebaseconfig";
 
 describe("TestimonialsComponent", () => {
   let component: TestimonialsComponent;
