f19f813464c5b33931a44d47163e700e2b972e90
Mick Evans <codemastermick@gmail.com>
2019-08-09 17:51:22 -0400

    feat: create testimonial service

--- src/app/shared/testimonials.service.ts
+++ src/app/shared/testimonials.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Testimonial } from 'app/pages/testimonials/testimonials.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TestimonialsService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  public getTestimonials() {
+    return this.db.collection("testimonials").valueChanges();
+  }
 }
