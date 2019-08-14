9f5042f3f8c2ac6bd02c610009e63901ffb55eb1
Mick Evans <codemastermick@gmail.com>
2019-08-14 10:53:25 -0400

    chore: remove dead code

--- src/app/shared/testimonials.service.spec.ts
+++ src/app/shared/testimonials.service.spec.ts
@@ -1,27 +1,10 @@
 import { TestBed } from "@angular/core/testing";
 
 import { TestimonialsService } from "./testimonials.service";
-import { Testimonial, TestimonialWithID } from "app/pages/testimonials/testimonials.component";
-import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
-import { BehaviorSubject, Observable, from } from "rxjs";
-import { map } from 'rxjs/operators';
+import { TestimonialWithID } from "app/pages/testimonials/testimonials.component";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { from } from "rxjs";
 
-// const FirestoreStub = {
-//   testimonials: {
-//     collection: (name: string) => ({
-//       doc: (id: "hn9324rh8f2") => ({
-//         valueChanges: () => new BehaviorSubject<TestimonialWithID>({
-//           id: "hn9324rh8f2",
-//           author: "Demo Bob",
-//           description: "Hello world",
-//           position: "CEO of nothing",
-//           done: false
-//         }),
-//         set: (data: any) => new Promise((resolve, reject) => resolve()),
-//       }),
-//     }),
-//   }
-// };
 
 const input: TestimonialWithID[] = [
   {
