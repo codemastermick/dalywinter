b2839687358e089f3470f7bcfcf95c62f62364c7
Mick Evans <codemastermick@gmail.com>
2019-08-21 14:34:03 -0400

    tests: get tests passing again

--- src/app/navigation/sidenav-list/sidenav-list.component.spec.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.spec.ts
@@ -2,22 +2,27 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { SidenavListComponent } from "./sidenav-list.component";
 import { MaterialModule } from "app/shared/material.module";
+import { MatSidenav } from "@angular/material/sidenav";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 describe("SidenavListComponent", () => {
   let component: SidenavListComponent;
   let fixture: ComponentFixture<SidenavListComponent>;
+  let sidenav: any;
   let spy: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SidenavListComponent],
-      imports: [MaterialModule]
+      imports: [MaterialModule, BrowserAnimationsModule]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SidenavListComponent);
     component = fixture.componentInstance;
+    sidenav = TestBed.createComponent(MatSidenav);
+    component.sidenav = sidenav;
     fixture.detectChanges();
   });
 
